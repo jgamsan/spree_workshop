45195a2c90a9f9b08006016bc7a21e3cdf408279
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-10-01 10:46:17 +0200

    Added fields

--- app/controllers/spree/workshops_controller.rb
+++ app/controllers/spree/workshops_controller.rb
@@ -1,4 +1,4 @@
-class Spree::WorkshopsController < Spree::ResourceController
+class Spree::WorkshopsController < Spree::BaseController
 
   def update_town_select
     towns = Spree::Town.where(:province_id => params[:id]).order(:name) unless params[:id].blank?

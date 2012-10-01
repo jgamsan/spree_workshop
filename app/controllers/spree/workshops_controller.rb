e8945024595331eb8a711cca97f2baa737cd9ad2
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-10-01 11:09:55 +0200

    Added fields

--- app/controllers/spree/workshops_controller.rb
+++ app/controllers/spree/workshops_controller.rb
@@ -1,6 +1,20 @@
 module Spree
   class WorkshopsController < BaseController
 
+    def create
+      @workshop = Spree::Workshop.new(params[:workshop])
+
+      if @contact.save
+        redirect_to('/', :notice => t('spree.contact_us.notices.success'))
+      else
+        render :new
+      end
+    end
+
+  def new
+    @workshop = Spree::Workshop.new
+  end
+
     def update_town_select
       towns = Spree::Town.where(:province_id => params[:id]).order(:name) unless params[:id].blank?
       render :partial => "towns", :locals => { :towns => towns}

c39bd523a3fe5b2b1f1a51c4263aad5ffee49b6b
jose miguel <miguel.gamazo@galiclick.com>
2012-10-01 16:54:43 +0200

    Better interface

--- app/models/spree/workshop.rb
+++ app/models/spree/workshop.rb
@@ -1,7 +1,8 @@
 module Spree
   class Workshop < ActiveRecord::Base
-    attr_accessible :name, :address, :town_id, :phone, :fax, :email, :notes, :price
-    cattr_accessor :province
+    attr_accessible :name, :address, :town_id, :phone, :fax, :email, :notes,
+                    :price_for_car, :price_for_moto
+    attr_accessor :province
 
     validates_format_of :email,
                         :with    => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,

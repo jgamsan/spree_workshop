5e6371da52b65796d5a05010e3c9aa3e72574cd4
jose miguel <miguel.gamazo@galiclick.com>
2012-10-01 17:34:14 +0200

    Better interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Spree::Core::Engine.routes.draw do
   resources :workshops
   match 'workshops/update_town_select/:id' => 'workshops#update_town_select'
+  namespace :admin do
+    resources :workshops
+  end
 end

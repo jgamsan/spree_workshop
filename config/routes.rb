2a4582601ed3cefc090841e65d7500c8f1807f99
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-10-03 21:09:02 +0200

    New Workshop search

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Spree::Core::Engine.routes.draw do
-  resources :workshops
+  resources :workshops do
+    collection do
+      get 'search'
+    end
+  end
   match 'workshops/update_town_select/:id' => 'workshops#update_town_select'
 
   namespace :admin do
-    resources :workshops do
-      collection do
-        get 'search'
-      end
-    end
+    resources :workshops
   end
 end

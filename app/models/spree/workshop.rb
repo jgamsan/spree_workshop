253684507b31eae85b362410d25d1875ed4b0df1
jose miguel <miguel.gamazo@galiclick.com>
2012-10-08 20:10:13 +0200

    Mas mejoras

--- app/models/spree/workshop.rb
+++ app/models/spree/workshop.rb
@@ -4,6 +4,7 @@ module Spree
                     :price_for_car, :price_for_moto, :province
     attr_accessor :province
     belongs_to :town
+    has_many :orders
     validates_format_of :email,
                         :with    => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
                         :message => "email incorrecto",
